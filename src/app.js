cde6224c3aa567cfb9f176c7e8ac35c61f788c1a
LeonardoCalonico <dcalonico2@gmail.com>
2025-12-20 00:20:10 +0000

    Generador excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+window.onload = function () {
+  //write your code here   
+  generadorDominio();
+}
+
+const generadorDominio = () => {
+    let pronoun = ['the', 'our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger', 'racoon'];
+    let dom = ['.com', '.es'];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      const pronombre = pronoun[i];
+
+      for (let ia = 0; ia < adj.length; ia++) {
+        const adjetivo = adj[ia];
+
+        for (let inon = 0; inon < noun.length; inon++) {
+
+          const sustativo = noun[inon];
+
+          for (let id = 0; id < dom.length; id++) {
+            const dominio = dom[id];
+
+            let combination = pronombre + adjetivo + sustativo + dominio;
+            console.log(combination)
+          }
+        }
+      }
+    }
+  }
+
+
