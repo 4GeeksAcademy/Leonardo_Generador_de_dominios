55fbecd8d2569b5175a9532f33574ea91180519a
LeonardoCalonico <dcalonico2@gmail.com>
2026-01-09 23:59:10 +0000

    coding guideline

--- src/app.js
+++ src/app.js
@@ -12,30 +12,25 @@ window.onload = function () {
 }
 
 const generadorDominio = () => {
-    let pronoun = ['the', 'our'];
-    let adj = ['great', 'big'];
-    let noun = ['jogger', 'racoon'];
-    let dom = ['.com', '.es'];
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let dom = ['.com', '.es'];
 
-    for (let i = 0; i < pronoun.length; i++) {
-      const pronombre = pronoun[i];
+  for (let i = 0; i < pronoun.length; i++) {
 
-      for (let ia = 0; ia < adj.length; ia++) {
-        const adjetivo = adj[ia];
+    for (let ia = 0; ia < adj.length; ia++) {
 
-        for (let inon = 0; inon < noun.length; inon++) {
+      for (let inon = 0; inon < noun.length; inon++) {
 
-          const sustativo = noun[inon];
+        for (let id = 0; id < dom.length; id++) {
 
-          for (let id = 0; id < dom.length; id++) {
-            const dominio = dom[id];
-
-            let combination = pronombre + adjetivo + sustativo + dominio;
-            console.log(combination)
-          }
+          let combination = pronoun[i] + adj[ia] + noun[inon] + dom[id];
+          console.log(combination)
         }
       }
     }
   }
+}
 
 
